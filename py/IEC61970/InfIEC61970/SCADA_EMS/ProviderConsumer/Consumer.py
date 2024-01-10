4decb59edc500e14b92f3e016e2b83caabba7e5c
PJMoran <pmoran@aptect.com>
2024-01-10 13:54:24 -0500

    Cleaning up the java code to remove python model files.

---
+++ py/IEC61970/InfIEC61970/SCADA_EMS/ProviderConsumer/Consumer.py
@@ -0,0 +1,20 @@
+# Converted by an OPENAI API call using model: gpt-3.5-turbo-1106 on Mon Jan  8 09:14:34 2024
+# Class for handling consumer behavior
+class MessagePayload:
+    pass
+
+
+class Consumer:
+    """
+    @author selaost1
+    @version 1.0
+    @created 25-Dec-2023 8:31:55 PM
+    """
+
+    def __init__(self):
+        pass
+        # Constructor to initialize the consumer
+
+    def reply(self, target_data: MessagePayload):
+        # Method to handle the reply of the consumer
+        pass
---
+++ py/IEC61970/InfIEC61970/SCADA_EMS/ProviderConsumer/Provider.py
@@ -0,0 +1,35 @@
+# Converted by an OPENAI API call using model: gpt-3.5-turbo-1106 on Mon Jan  8 09:14:34 2024
+from IEC61970.InfIEC61970.SCADA_EMS.ProviderConsumer.Consumer import Consumer
+
+
+class Provider:
+    """
+    @author selaost1
+    @version 1.0
+    @created 25-Dec-2023 8:32:02 PM
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.m_consumer = Consumer()  # Initializing m_consumer attribute to the Consumer class
+        # public Consumer m_Consumer;
+
+    """
+    * 
+    * @param target
+    */
+    """
+    def execute(self, target):
+        pass
+        # public execute(MessagePayload target){
+
+    """
+    * 
+    * @param target
+    */
+    """
+    def get(self, target):
+        pass
+        # public get(MessagePayload target){
+        
+# end Provider
